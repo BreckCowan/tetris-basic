5cf9735cab194b98c746d319ac7eb32c21fa043f
Breck Cowan <breckcowan@gmail.com>
2022-06-11 12:14:14 -0500

    added colors to different tetrominoes

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
   let nextRandom = 0;
   let timerId;
   let score = 0;
+  const colors = [
+    'blue',
+    'red',
+    'purple',
+    'green',
+    'orange'
+  ]
 
   //The Tetrominoes
   const lTetromino = [
@@ -66,6 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function draw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.add("tetromino");
+      squares[currentPosition + index].style.backgroundColor = colors[random]
     });
   }
 
@@ -73,6 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function undraw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.remove("tetromino");
+      squares[currentPosition + index].style.backgroundColor = '';
     });
   }
 
@@ -174,7 +183,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //show next tetromino in mini-grid
   const displaySquares = document.querySelectorAll(".mini-grid div");
   const displayWidth = 4;
-  let displayIndex = 0;
+  const displayIndex = 0;
 
   //the tetrominoes without rotations
   const upNextTetrominoes = [
@@ -189,9 +198,11 @@ document.addEventListener("DOMContentLoaded", () => {
   function displayShape() {
     displaySquares.forEach((square) => {
       square.classList.remove("tetromino");
+      square.style.backgroundColor = '';
     });
     upNextTetrominoes[nextRandom].forEach((index) => {
       displaySquares[displayIndex + index].classList.add("tetromino");
+      displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
     });
   }
 
@@ -219,6 +230,7 @@ document.addEventListener("DOMContentLoaded", () => {
         row.forEach(index => {
           squares[index].classList.remove('taken')
           squares[index].classList.remove('tetromino')
+          squares[index].style.backgroundColor = ''
         })
         const squaresRemoved = squares.splice(i, width)
         squares = squaresRemoved.concat(squares)
